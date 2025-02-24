47eb51fb93ec8cd4a7622b33ec4587c08a4b935f
Vikash4110 <139474262+Vikash4110@users.noreply.github.com>
2025-02-25 03:47:51 +0530

    main

--- server/server.js
+++ server/server.js
@@ -166,7 +166,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173", "https://margdakshi.vercel.app", "http://localhost:8000"],
+    origin: ["http://localhost:5173", "https://margdarshi.vercel.app", "http://localhost:8000"],
     methods: ["GET", "POST"],
   },
 });
